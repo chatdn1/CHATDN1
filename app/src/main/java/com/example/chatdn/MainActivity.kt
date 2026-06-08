8ca986732ec3d1a2bad04bbef5ada1e64d7e3d61
chatdn1 <lotefellah99@gmail.com>
2026-06-08 15:48:58 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/chatdn/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.chatdn
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://chat.openai.com")
+    }
+}
